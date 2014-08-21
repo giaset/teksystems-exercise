63361b668f2898bb12ca0dbf6df562c837fe1aa0
Gianni Settino <g@miltonandparc.com>
2014-08-21 01:11:59 -0400

    Manage entering and exiting fullscreen mode

--- teksystems-exercise/ViewControllers/TEKMapViewController.swift
+++ teksystems-exercise/ViewControllers/TEKMapViewController.swift
@@ -18,7 +18,11 @@ class TEKMapViewController: UIViewController, MKMapViewDelegate, UIScrollViewDel
     let locationManager = CLLocationManager()
     
     var mapview: MKMapView?
+    var mapViewBottomBorder = CALayer()
+    
     var tableview: UITableView?
+    
+    var isFullscreen = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,7 +49,7 @@ class TEKMapViewController: UIViewController, MKMapViewDelegate, UIScrollViewDel
     func addButtonPressed() {
         
     }
-    
+
     func setupMapView() {
         mapview = MKMapView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
         mapview!.scrollEnabled = false
@@ -55,21 +59,71 @@ class TEKMapViewController: UIViewController, MKMapViewDelegate, UIScrollViewDel
         mapview!.showsUserLocation = true
         
         // Add a bottom border to the mapView to separate it from the tableView
-        var bottomBorder = CALayer()
         var bottomBorderThickness: CGFloat = 1
-        bottomBorder.frame = CGRect(x: 0, y: heightOfExposedMapView-bottomBorderThickness, width: self.view.frame.width, height: bottomBorderThickness)
-        bottomBorder.backgroundColor = UIColor.lightGrayColor().CGColor
-        mapview!.layer.addSublayer(bottomBorder)
+        mapViewBottomBorder.frame = CGRect(x: 0, y: heightOfExposedMapView-bottomBorderThickness, width: self.view.frame.width, height: bottomBorderThickness)
+        mapViewBottomBorder.backgroundColor = UIColor.lightGrayColor().CGColor
+        mapview!.layer.addSublayer(mapViewBottomBorder)
         
         self.view.addSubview(mapview)
+        
+        // When the mapView is tapped, go fullscreen. Use a dummy view for this, because the mapView has userInteractionEnabled set to 'false'
+        var dummyView = UIView(frame: mapview!.frame)
+        var singleTap = UITapGestureRecognizer(target: self, action: "goFullscreen")
+        dummyView.addGestureRecognizer(singleTap)
+        
+        self.view.addSubview(dummyView)
     }
     
     func setupTableView() {
-        tableview = UITableView(frame: CGRect(x: 0, y: heightOfExposedMapView, width: self.view.frame.width, height: self.view.frame.height-heightOfExposedMapView))
+        tableview = UITableView(frame: CGRect(x: 0, y: heightOfExposedMapView, width: self.view.frame.width, height: self.view.frame.height-heightOfExposedMapView+50))
         tableview!.delegate = self
         tableview!.showsVerticalScrollIndicator = false
         
         self.view.addSubview(tableview)
+    }
+    
+    func goFullscreen() {
+        if (!isFullscreen) {
+            // First, hide the custom border we have drawn over the mapView
+            // We use removeFromSuperLayer first because modifying 'hidden' or 'opacity' seems to be animated by default...
+            mapViewBottomBorder.removeFromSuperlayer()
+            mapViewBottomBorder.hidden = true
+            
+            // Next, animate the tableView dropping down out of sight
+            setTableViewYTo(self.view.frame.height, onComplete: {
+                didFinish in
+                if (didFinish) {
+                    self.isFullscreen = true
+                }
+                })
+            
+            // Lastly, give us a back button to exit fullscreen mode
+            var backButton = UIBarButtonItem(title: "Close", style: .Bordered, target: self, action: "exitFullscreen")
+            self.navigationItem.leftBarButtonItem = backButton
+        }
+    }
+    
+    func exitFullscreen() {
+        if (isFullscreen) {
+            // Bring the tableView back to its original position
+            setTableViewYTo(heightOfExposedMapView, onComplete: {
+                didFinish in
+                if (didFinish) {
+                    self.isFullscreen = false
+                    
+                    // Show the mapView's bottom Border again
+                    self.mapview!.layer.addSublayer(self.mapViewBottomBorder)
+                    self.mapViewBottomBorder.hidden = false
+                }
+                })
+            
+            // Hide the back button when not in fullscreen mode
+            self.navigationItem.leftBarButtonItem = nil
+        }
+    }
+    
+    func setTableViewYTo(newY: CGFloat, onComplete: (Bool) -> ()) {
+        UIView.animateWithDuration(0.7, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0, options: nil, animations: { self.tableview!.frame.origin.y = newY }, completion: onComplete)
     }
     
 }
